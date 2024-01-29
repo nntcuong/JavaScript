633c07db5a2160a6cfcfe68bf386fd9e0a9e7735
nntcuong <thecuongyt3htk@gmail.com>
2024-01-29 21:47:57 +0700

    first

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -2,5 +2,6 @@ const todoList=[];
 function addTodo(){
     const inputElement=document.querySelector('.js-name-input');
     const name = inputElement.value;
-    console.log(name);
+    todoList.push(name);
+    console.log(todoList)
 }
